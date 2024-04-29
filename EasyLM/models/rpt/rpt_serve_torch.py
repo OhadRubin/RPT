9c0dfbdded69e813f2ee31a8d005504982787db2
Shahar Satamkar <shahar.satamkar@fiverr.com>
2024-04-29 17:55:39 +0300

    a smooth solution

--- EasyLM/models/rpt/rpt_serve_torch.py
+++ EasyLM/models/rpt/rpt_serve_torch.py
@@ -326,6 +326,7 @@ def main(argv):
     hf_model = RPTForCausalLM(rpt_config, device=device)
     hf_model.to(device)
 
+    """
     from transformers.modeling_flax_pytorch_utils import load_flax_weights_in_pytorch_model
 
     for key_to_modify in ['lowcoder', 'upcoder']:
@@ -336,6 +337,7 @@ def main(argv):
     load_flax_weights_in_pytorch_model(hf_model, params['params'])
 
     hf_model.save_pretrained('rpt-torch-1')
+    """
     hf_model.from_pretrained('rpt-torch-1')
 
     _forward_upcoder = apply_forward_upcoder
